efe10fa0cef8038081e25af219c6010d072a938c
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 17:06:46 +0200

    Added ability to get all notes from db

--- app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
@@ -1,17 +1,43 @@
 package com.tsekhmeistruk.notary.notes
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.widget.Toast
 import com.tsekhmeistruk.notary.R
 import com.tsekhmeistruk.notary.addeditnote.AddEditNoteFragment
 import com.tsekhmeistruk.notary.util.BaseActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_note_list.*
+import javax.inject.Inject
 
 class NoteListActivity : BaseActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private lateinit var viewModel: NotesViewModule
+
+    private val disposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
+        getAppComponent().inject(this)
+
         add_button.setOnClickListener { startFragment(AddEditNoteFragment.newInstance(), true) }
     }
+
+    /*override fun onStart() {
+        super.onStart()
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(NotesViewModule::class.java)
+
+        // TODO
+        disposable.add(viewModel.getAllNotes()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe())
+    }*/
 }
--- app/src/main/java/com/tsekhmeistruk/notary/notes/NotesViewModule.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/notes/NotesViewModule.kt
@@ -1,10 +1,16 @@
 package com.tsekhmeistruk.notary.notes
 
 import android.arch.lifecycle.ViewModel
+import com.tsekhmeistruk.notary.data.Note
 import com.tsekhmeistruk.notary.data.source.NotesRepository
+import io.reactivex.Flowable
 
 /**
  * Created by Roman Tsekhmeistruk on 09.03.2018.
  */
 class NotesViewModule(private var notesRepository: NotesRepository) : ViewModel() {
+
+    fun getAllNotes(): Flowable<List<Note>> {
+        return notesRepository.getAllNotes()
+    }
 }
