4dcf302283a45466a02d02ff0928b7bbd896b7e4
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 10:50:54 +0200

    Initial commit

---
+++ app/src/main/java/com/tsekhmeistruk/notary/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.tsekhmeistruk.notary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
