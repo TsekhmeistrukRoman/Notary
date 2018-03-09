8238dd0b4ff0cd610cc9a1eaea186f392af228c6
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 17:36:23 +0200

    Added BaseEntity class

---
+++ app/src/main/java/com/tsekhmeistruk/notary/data/BaseEntity.kt
@@ -0,0 +1,7 @@
+package com.tsekhmeistruk.notary.data
+
+/**
+ * Created by Roman Tsekhmeistruk on 09.03.2018.
+ */
+open class BaseEntity {
+}
--- app/src/main/java/com/tsekhmeistruk/notary/data/Note.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/data/Note.kt
@@ -13,4 +13,4 @@ class Note constructor(
         @ColumnInfo(name = "title") var title: String = "",
         @ColumnInfo(name = "date") var date: String = "",
         @ColumnInfo(name = "colorRes") var colorResource: Int = 0,
-        @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString())
+        @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()) : BaseEntity()
