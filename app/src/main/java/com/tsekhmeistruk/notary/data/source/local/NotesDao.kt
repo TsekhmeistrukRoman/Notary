5fe135b198bad21cfae6e5e1814f81a1ba70ce78
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 11:59:26 +0200

    Added notes dao room interface

---
+++ app/src/main/java/com/tsekhmeistruk/notary/data/source/local/NotesDao.kt
@@ -0,0 +1,27 @@
+package com.tsekhmeistruk.notary.data.source.local
+
+import android.arch.persistence.room.*
+import com.tsekhmeistruk.notary.data.Note
+import io.reactivex.Flowable
+
+/**
+ * Created by Roman Tsekhmeistruk on 09.03.2018.
+ */
+@Dao
+interface NotesDao {
+
+    @Query("SELECT * FROM Notes")
+    fun getAllNotes(): Flowable<List<Note>>
+
+    @Query("SELECT * FROM Notes WHERE entryid = :noteId")
+    fun getNoteById(noteId: String): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: Note)
+
+    @Update
+    fun updateNote(note: Note): Int
+
+    @Query("DELETE FROM Notes WHERE entryid = :noteId")
+    fun deleteNoteById(noteId: String): Int
+}
