0ed1b8ad98a31d421c09939d441363e9b22d1ec8
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 11:04:45 +0200

    Added note list screen design

--- app/src/main/java/com/tsekhmeistruk/notary/NoteListActivity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/NoteListActivity.kt
@@ -3,10 +3,10 @@ package com.tsekhmeistruk.notary
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LaunchActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launch)
+        setContentView(R.layout.note_list_launch)
     }
 }
