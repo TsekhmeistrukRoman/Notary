d2ebe53ae3bd25dade8aa8d8d2050e2a6b0bf3ee
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 11:10:51 +0200

    Renamed layout file of note list activity and changed it's background color

--- app/src/main/java/com/tsekhmeistruk/notary/NoteListActivity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/NoteListActivity.kt
@@ -7,6 +7,6 @@ class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.note_list_launch)
+        setContentView(R.layout.activity_note_list)
     }
 }
