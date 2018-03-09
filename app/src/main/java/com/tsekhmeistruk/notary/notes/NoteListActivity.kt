57cfb7ca175871366c6cba7fae958a7ac695f636
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 15:07:05 +0200

    Added AddEditFragment starting when "add" button click

--- app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
@@ -1,13 +1,17 @@
 package com.tsekhmeistruk.notary.notes
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.tsekhmeistruk.notary.R
+import com.tsekhmeistruk.notary.addeditnote.AddEditFragment
+import com.tsekhmeistruk.notary.util.BaseActivity
+import kotlinx.android.synthetic.main.activity_note_list.*
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        add_button.setOnClickListener { startFragment(AddEditFragment.newInstance(), true) }
     }
 }
