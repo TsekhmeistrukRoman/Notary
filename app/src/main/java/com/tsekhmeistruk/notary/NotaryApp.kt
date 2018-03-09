a920f24c9d18019ba9dd694f04a62bef6ed8ae64
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 11:14:15 +0200

    Added application class

---
+++ app/src/main/java/com/tsekhmeistruk/notary/NotaryApp.kt
@@ -0,0 +1,13 @@
+package com.tsekhmeistruk.notary
+
+import android.app.Application
+
+/**
+ * Created by Roman Tsekhmeistruk on 09.03.2018.
+ */
+class NotaryApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
