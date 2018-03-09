50a3feeb27851dc5878a2e42bdcf28981704c0b0
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-09 12:29:28 +0200

    Added notes repository

---
+++ app/src/main/java/com/tsekhmeistruk/notary/data/source/NotesRepository.kt
@@ -0,0 +1,33 @@
+package com.tsekhmeistruk.notary.data.source
+
+import com.tsekhmeistruk.notary.data.Note
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
+import javax.inject.Inject
+
+/**
+ * Created by Roman Tsekhmeistruk on 09.03.2018.
+ */
+class NotesRepository @Inject constructor(private val notesLocalDataSource: NotesDataSource) : NotesDataSource {
+
+    override fun getAllNotes(): Flowable<List<Note>> {
+        return notesLocalDataSource.getAllNotes()
+    }
+
+    override fun getNoteById(noteId: String): Single<Note> {
+        return notesLocalDataSource.getNoteById(noteId)
+    }
+
+    override fun removeNote(noteId: String): Completable {
+        return notesLocalDataSource.removeNote(noteId)
+    }
+
+    override fun updateNote(note: Note): Completable {
+        return notesLocalDataSource.updateNote(note)
+    }
+
+    override fun addNote(note: Note): Completable {
+        return notesLocalDataSource.addNote(note)
+    }
+}
