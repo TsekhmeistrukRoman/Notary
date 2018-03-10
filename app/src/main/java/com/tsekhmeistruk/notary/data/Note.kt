ca852e31e717d92e27b60509c2f4cb844780d902
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-10 17:44:47 +0200

    Implemented hashCode and equals methods of Note data class

--- app/src/main/java/com/tsekhmeistruk/notary/data/Note.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/data/Note.kt
@@ -14,4 +14,29 @@ class Note constructor(
         @ColumnInfo(name = "date") var date: String = "",
         @ColumnInfo(name = "colorRes") var colorResource: Int = 0,
         @ColumnInfo(name = "pagerPosition") var pagerPosition: Int = 0,
-        @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()) : BaseEntity()
+        @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()) : BaseEntity() {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (title != other.title) return false
+        if (date != other.date) return false
+        if (colorResource != other.colorResource) return false
+        if (pagerPosition != other.pagerPosition) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + date.hashCode()
+        result = 31 * result + colorResource
+        result = 31 * result + pagerPosition
+        result = 31 * result + id.hashCode()
+        return result
+    }
+}
