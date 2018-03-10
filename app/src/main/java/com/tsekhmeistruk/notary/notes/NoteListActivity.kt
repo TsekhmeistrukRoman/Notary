e4f5f6ead3ca9f1d039940aeac92e58a463284a5
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-10 16:52:29 +0200

    Added notes loading from database and displaying in the screen

--- app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/notes/NoteListActivity.kt
@@ -1,11 +1,18 @@
 package com.tsekhmeistruk.notary.notes
 
 import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import com.tsekhmeistruk.notary.R
 import com.tsekhmeistruk.notary.addeditnote.AddEditNoteFragment
+import com.tsekhmeistruk.notary.data.Note
+import com.tsekhmeistruk.notary.widgets.Status
+import com.tsekhmeistruk.notary.widgets.adapters.NoteListAdapter
 import com.tsekhmeistruk.notary.widgets.util.BaseActivity
-import io.reactivex.disposables.CompositeDisposable
+import com.tsekhmeistruk.notary.widgets.util.DataResource
 import kotlinx.android.synthetic.main.activity_note_list.*
 import javax.inject.Inject
 
@@ -15,7 +22,7 @@ class NoteListActivity : BaseActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: NotesViewModule
 
-    private val disposable = CompositeDisposable()
+    private lateinit var listAdapter: NoteListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +31,30 @@ class NoteListActivity : BaseActivity() {
         getAppComponent().inject(this)
 
         add_button.setOnClickListener { startFragment(AddEditNoteFragment.newInstance(), true) }
-    }
 
-    /*override fun onStart() {
-        super.onStart()
+        listAdapter = NoteListAdapter()
+        note_list.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        note_list.itemAnimator = DefaultItemAnimator()
+        note_list.adapter = listAdapter
+
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(NotesViewModule::class.java)
+        viewModel.getLiveData().observe(this, android.arch.lifecycle.Observer<DataResource<List<Note>>> { res ->
+            if (res != null) {
+                when (res.status) {
+                    Status.LOADING -> {
 
-        // TODO
-        disposable.add(viewModel.getAllNotes()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe())
-    }*/
+                    }
+                    Status.ERROR -> {
+                        Toast.makeText(applicationContext, getText(R.string.error), Toast.LENGTH_LONG).show()
+                        listAdapter.clearList()
+                    }
+                    Status.SUCCESS -> {
+                        listAdapter.add(res.data as List<Note>)
+                    }
+                }
+            }
+        })
+
+        viewModel.getAllNotes()
+    }
 }
--- app/src/main/java/com/tsekhmeistruk/notary/notes/NotesViewModule.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/notes/NotesViewModule.kt
@@ -1,16 +1,51 @@
 package com.tsekhmeistruk.notary.notes
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.MainThread
+import android.util.Log
 import com.tsekhmeistruk.notary.data.Note
 import com.tsekhmeistruk.notary.data.source.NotesRepository
-import io.reactivex.Flowable
+import com.tsekhmeistruk.notary.widgets.util.DataResource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 /**
  * Created by Roman Tsekhmeistruk on 09.03.2018.
  */
 class NotesViewModule(private var notesRepository: NotesRepository) : ViewModel() {
 
-    fun getAllNotes(): Flowable<List<Note>> {
-        return notesRepository.getAllNotes()
+    private val tag = "NotesViewModule"
+
+    private val compositeDisposable = CompositeDisposable()
+    private val liveData = MutableLiveData<DataResource<List<Note>>>()
+
+    fun getAllNotes() {
+        compositeDisposable.add(notesRepository.getAllNotes()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ setValue(DataResource.success(it)) },
+                        {
+                            Log.e(tag, it.message)
+                            setValue(DataResource.error(it, null))
+                        }))
+    }
+
+    @MainThread
+    private fun setValue(newValue: DataResource<List<Note>>) {
+        if (!Objects.equals(liveData.value, newValue)) {
+            liveData.value = newValue
+        }
+    }
+
+    fun getLiveData(): MutableLiveData<DataResource<List<Note>>> {
+        return liveData
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
     }
 }
