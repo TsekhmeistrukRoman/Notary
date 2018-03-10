12328f601f3507a0d09564afcf31b019d4ad78fc
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-03-10 16:44:30 +0200

    BaseEntity class implements Serializable interface

--- app/src/main/java/com/tsekhmeistruk/notary/data/BaseEntity.kt
+++ app/src/main/java/com/tsekhmeistruk/notary/data/BaseEntity.kt
@@ -1,7 +1,9 @@
 package com.tsekhmeistruk.notary.data
 
+import java.io.Serializable
+
 /**
  * Created by Roman Tsekhmeistruk on 09.03.2018.
  */
-open class BaseEntity {
+open class BaseEntity : Serializable {
 }
